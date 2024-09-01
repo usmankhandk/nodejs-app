de1bfcf1b53902150f0b4159be40092be302d0f4
usmannovasol <118891379+usmannovasol@users.noreply.github.com>
2024-09-01 15:12:06 +0200

    Resolved merge conflicts

---
+++ test/cdk-nodejs-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkNodejsPipeline from '../lib/cdk-nodejs-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-nodejs-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkNodejsPipeline.CdkNodejsPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
